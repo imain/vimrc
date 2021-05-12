25c2331d4be6a32b839af30047b2a9e0872e4384
Ian Main <imain@ona.mains.priv>
2021-05-11 18:07:55 -0700

    So I can remember how to do it..

---
+++ gnome_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['F1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['F2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['F3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['F4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['F5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['F6']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['F7']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['F8']"
+
